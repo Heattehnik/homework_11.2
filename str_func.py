ee53dfeb77144a6e67f01918caa5f88f970dd01e
Heattehnik <gadjka@mail.ru>
2023-03-13 16:12:56 +0500

    refactor str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_func(string: str) -> str:
     return string.upper()
+
+
+def first_upper(string: str) -> str:
+    """
+    Function get string and return string with upper case first letter of each word
+    :param string:
+    :return:
+    """
+    return string.title()
